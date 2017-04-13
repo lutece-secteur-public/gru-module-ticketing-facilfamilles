3a39f0619939b24c408bbaba542b192cab82f47d
FLAtos35 <fabrice.leac@atos.net>
2017-04-13 14:17:02 +0200

    TICKETFF-26 : Add recipients and cc in history of task "Solliciter acteur terrain"

--- src/sql/plugins/workflow/modules/ticketingfacilfamilles/upgrades/update_db_workflow-ticketing-facilfamilles-1.0.3-1.0.4.sql
+++ src/sql/plugins/workflow/modules/ticketingfacilfamilles/upgrades/update_db_workflow-ticketing-facilfamilles-1.0.3-1.0.4.sql
@@ -1,3 +1,32 @@
+/*=============================================================*/
+/* Table struture for table workflow_task_ticketing_facilfamilles_emailagent_recipient */
+/*=============================================================*/
+DROP TABLE IF EXISTS workflow_task_ticketing_facilfamilles_emailagent_recipient;
+CREATE TABLE workflow_task_ticketing_facilfamilles_emailagent_recipient
+(
+	id_recipient INT DEFAULT 0 NOT NULL,
+	id_task INT DEFAULT 0 NOT NULL,
+	id_history INT DEFAULT 0 NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	field VARCHAR(255) DEFAULT NULL,
+	name VARCHAR(255) DEFAULT NULL,
+	firstname VARCHAR(255) DEFAULT NULL,
+	PRIMARY KEY (id_recipient)
+);
+
+/*=============================================================*/
+/* Table struture for table workflow_task_ticketing_facilfamilles_emailagent_cc */
+/*=============================================================*/
+DROP TABLE IF EXISTS workflow_task_ticketing_facilfamilles_emailagent_cc;
+CREATE TABLE workflow_task_ticketing_facilfamilles_emailagent_cc
+(
+	id_cc INT DEFAULT 0 NOT NULL,
+	id_task INT DEFAULT 0 NOT NULL,
+	id_history INT DEFAULT 0 NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id_cc)
+);
+
 ALTER TABLE ticketing_facilfamilles_emailagent CHANGE email_agent email_recipients MEDIUMTEXT DEFAULT NULL;
 ALTER TABLE ticketing_facilfamilles_emailagent ADD COLUMN email_recipients_cc MEDIUMTEXT DEFAULT NULL;
 
