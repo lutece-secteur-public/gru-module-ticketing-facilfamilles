a37e25013171ccf4a2403ff714cd669e377eade1
TrainE <erwan.train@atos.net>
2017-04-28 16:53:33 +0200

    TICKETFF-28 : Move an sql update to the current version from a previous script to the current upgrade script

--- src/sql/plugins/workflow/modules/ticketingfacilfamilles/upgrades/update_db_workflow-ticketing-facilfamilles-1.0.3-1.0.4.sql
+++ src/sql/plugins/workflow/modules/ticketingfacilfamilles/upgrades/update_db_workflow-ticketing-facilfamilles-1.0.3-1.0.4.sql
@@ -29,5 +29,3 @@ CREATE TABLE workflow_task_ticketing_facilfamilles_emailagent_cc
 
 ALTER TABLE ticketing_facilfamilles_emailagent CHANGE email_agent email_recipients MEDIUMTEXT DEFAULT NULL;
 ALTER TABLE ticketing_facilfamilles_emailagent ADD COLUMN email_recipients_cc MEDIUMTEXT DEFAULT NULL;
-
-UPDATE workflow_task_notify_gru_cf SET email_broadcast = '${email_recipients!}', recipients_cc_broadcast = '${email_recipients_cc!}' WHERE id_spring_provider = 'workflow-ticketingfacilfamilles.provider-manager.@.ticket';
--- src/sql/plugins/workflow/modules/ticketingfacilfamilles/upgrades/update_db_workflow-ticketing-facilfamilles-1.0.4-1.0.5.sql
+++ src/sql/plugins/workflow/modules/ticketingfacilfamilles/upgrades/update_db_workflow-ticketing-facilfamilles-1.0.4-1.0.5.sql
@@ -11,3 +11,5 @@ DROP TABLE IF EXISTS ticketing_facilfamilles_emailagent;
 DROP TABLE IF EXISTS workflow_task_ticketing_facilfamilles_emailagent_config;
 DROP TABLE IF EXISTS workflow_task_ticketing_facilfamilles_emailagent_recipient;
 DROP TABLE IF EXISTS workflow_task_ticketing_facilfamilles_emailagent_cc;
+
+UPDATE workflow_task_notify_gru_cf SET email_broadcast = '${email_recipients!}', recipients_cc_broadcast = '${email_recipients_cc!}' WHERE id_spring_provider = 'workflow-ticketingfacilfamilles.provider-manager.@.ticket';
